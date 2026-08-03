c4aa211e0b738335dbb64f2c3335cc9fa039e67c
cjy00852 <chlwldud1992@gmail.com>
2026-08-03 14:55:47 +0900

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.scenebox.rescene"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.scenebox.rescene"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
