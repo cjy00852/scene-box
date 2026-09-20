5e05accd305cb5807a2881b5fa09deb966bd462d
cjy00852 <chlwldud1992@gmail.com>
2026-09-20 16:06:29 +0900

    Configure Google OAuth web client ID

--- google-config.js
+++ google-config.js
@@ -1,3 +1,3 @@
 // Google Cloud > Google Auth Platform > Clients > Web application client ID.
 // Public configuration only. Never put a client secret here.
-window.SCENE_GOOGLE_CLIENT_ID = "";
+window.SCENE_GOOGLE_CLIENT_ID = "133537057642-8redcbb00fq3jc93uf7sqfv42idkmjj3.apps.googleusercontent.com";
